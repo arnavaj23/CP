9d084baf00c5a319d43a9224aff93a3c375bd3ee
arnavaj23 <147527837+arnavaj23@users.noreply.github.com>
2024-12-14 02:55:20 +0530

    Create LCM_HCF.cpp

---
+++ General functions/LCM_HCF.cpp
@@ -0,0 +1,14 @@
+int HCF(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int LCM(int a, int b) {
+    return (a * b) / calculateHCF(a, b);
+}
+
+
