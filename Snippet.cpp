aad2ab87afb5b49ac63453426572d369a78035b6
arnavaj23 <147527837+arnavaj23@users.noreply.github.com>
2025-02-18 11:27:23 +0530

    Update Snippet.cpp
    included debugging

--- Snippet.cpp
+++ Snippet.cpp
@@ -1,24 +1,35 @@
 //                                            ||  ॐ गं गणपतये नमः  ||
 
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std; 
+
+// debugging -------------------------------------------------------------------------------------------------------------------------------
+#define LOCAL
+#ifdef LOCAL
+#define dbg(x) cerr << "Ln " << __LINE__ << ": " << #x << " = " << (x) << endl;
+#define dbgarr(arr) cerr << "Ln " << __LINE__ << ": " << #arr << " = ["; for (auto &i : arr) cerr << i << " "; cerr << "]" << endl;
+#else
+#define dbg(x)
+#define dbgarr(arr)
+#endif
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 typedef long long ll;
-typedef vector<long long> vi;
+typedef vector<ll> vi;
 typedef vector<vector<ll>> vvi;
-typedef pair<long long,ll> pii ;
+typedef pair<ll,ll> pii ;
 #define f(i,a,b) for( ll i=a;i<b;i++)
 #define PB push_back
 #define MP make_pair
+#define all(v) (v).begin(), (v).end()
+const int mod=1e9+7;
 
 template<typename T>
 void vin(ll a,vector<T> &v){
-    ll x=0;
     f(i,0,a){
         T ele;
-        cin>>ele;
-        x+=ele;
-        v.PB(x);
+        cin>>ele;    
+        v.PB(ele);
     }
 }
 
@@ -28,27 +39,17 @@ void vout(vector<T> &v){
         cout<<v[i]<<" ";
     }
 }
-vector<ll> findFactors(int n) {
-    vector<ll> factors;
-   
-    for (ll i = 1; i * i <= n; ++i) {
-        if (n % i == 0) {
-            factors.push_back(i);
-            if (i != n / i) {
-                factors.push_back(n / i); 
-            }
-        }
-    }
-    
-    return factors;
-}
+//----------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------
 int main(){
 ios::sync_with_stdio(0);
     cin.tie(0);
     ll t;
     cin>>t;
     while(t--){
-    
+        
     }
 return 0;
 }
+//-----------------------------------------------------------------------------------------------------------
