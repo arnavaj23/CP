a0818facb9b1e3c0c451e211e949e2e7b6c2dc69
arnavaj23 <147527837+arnavaj23@users.noreply.github.com>
2025-02-20 02:36:50 +0530

    Create prime_factor_and_their_power.cpp

---
+++ General functions/prime_factor_and_their_power.cpp
@@ -0,0 +1,13 @@
+void add_divs(int x, map<int, int>&divs){
+    int i = 2;
+    while(i * i <= x){
+        while (x % i == 0){
+            divs[i]++;
+            x /= i;
+        }
+        i++;
+    }
+    if(x > 1) divs[x]++;
+}
+
+// adds all prime factors anfd their powers in map divs
